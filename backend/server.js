7cc578369b06393c73ef972e8a96354a3452ac27
TrishLeaf <trishanp96@gmail.com>
2022-08-09 22:33:24 +0530

    user route modified

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 const userRouter = require('./routes/userRouter')
 
 //express app
