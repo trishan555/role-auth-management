c0be888910bbbe8310fbb839fcc2a1fcd05a2245
TrishLeaf <trishanp96@gmail.com>
2022-08-12 15:29:44 +0530

    Admin home page done

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useCookies } from 'react-cookie'
+import { Toaster, toast } from 'react-hot-toast'
 import axios from 'axios'
 import {
     Navbar,
     Table,
     Button,
-    ButtonGroup,
     Container,
     Nav,
     Form,
@@ -14,6 +14,7 @@ import {
 } from 'react-bootstrap'
 import ViewUserModel from './ViewUserModel'
 import jwt_decode from 'jwt-decode'
+import { CgTrash } from 'react-icons/cg'
 
 export default function AdminHome() {
     //cookie decode
@@ -41,7 +42,6 @@ export default function AdminHome() {
                 }
             )
             setUsers(result.data.users)
-            // setPages(result.data.pages)
         } catch (error) {
             if (
                 error.message.includes('401') ||
@@ -51,6 +51,18 @@ export default function AdminHome() {
                 return false
             }
         }
+    }
+
+    const deleteUser = (id) => {
+        axios
+            .delete(`http://localhost:8000/user/delete/${id}`)
+            .then(() => {
+                toast.success('Student successfully deleted !')
+                fetchUsers()
+            })
+            .catch((error) => {
+                console.log(error)
+            })
     }
 
     //handling search
@@ -75,14 +87,6 @@ export default function AdminHome() {
         }
     }
 
-    // page handling
-    // const nextPage = () => {
-    //     if (pages.totalPages > page) setPage(++page)
-    // }
-    // const previousPage = () => {
-    //     if (1 < page) setPage(--page)
-    // }
-
     useEffect(() => {
         fetchUsers()
     }, [])
@@ -93,102 +97,103 @@ export default function AdminHome() {
     }
 
     return (
-        <div>
-            <Navbar bg='dark' variant='dark'>
-                <Container>
-                    <Navbar.Brand>Note App | Admin Panel</Navbar.Brand>
-                    <Nav defaultActiveKey='/admin' className='me-auto'>
-                        <Nav.Link href='/admin'>Home</Nav.Link>
-                        <Nav.Link href='/admin/create'>Create User</Nav.Link>
-                        <Nav.Link onClick={logOut}>
-                            Log Out ({decoded.email})
-                        </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-
-            <div className='d-flex  justify-content-center'>
-                <h2 className='m-3'>All Users</h2>
+        <>
+            <div>
+                <Toaster position='top-center' reverseOrder={false} />
             </div>
+            <div>
+                <Navbar bg='dark' variant='dark'>
+                    <Container>
+                        <Navbar.Brand>WasToWill</Navbar.Brand>
+                        <Nav defaultActiveKey='/admin' className='me-auto'>
+                            <Nav.Link href='/admin'>Home</Nav.Link>
+                            <Nav.Link href='/admin/create'>
+                                Create User
+                            </Nav.Link>
+                            <Nav.Link onClick={logOut}>
+                                Log Out ({decoded.email})
+                            </Nav.Link>
+                        </Nav>
+                    </Container>
+                </Navbar>
 
-            <div className='d-flex flex-wrap justify-content-center'>
-                <Table striped bordered hover style={{ width: '75vw' }}>
-                    <thead>
-                        <tr>
-                            <th className='text-center'>First Name</th>
-                            <th className='text-center'>Last Name</th>
-                            <th className='text-center'>Email</th>
-                            <th className='text-center'>Phone Number</th>
-                            <th className='text-center'>Birth Day</th>
-                            <th className='text-center'>
-                                <Form>
-                                    <Row>
-                                        <Col xs={10}>
-                                            <Form.Control
-                                                onChange={(e) =>
-                                                    handleSearch(e)
-                                                }
-                                                name='item'
-                                                placeholder='Search by email'
-                                            />
-                                        </Col>
-                                    </Row>
-                                </Form>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {users.map((user) => (
-                            <tr key={user._id}>
-                                <td className='text-center'>
-                                    {user.firstName}
-                                </td>
-                                <td className='text-center'>{user.lastName}</td>
-                                <td className='text-center'>{user.email}</td>
-                                <td className='text-center'>{user.phone}</td>
-                                <td className='text-center'>
-                                    {dateConvert(user.dateOfBirth)}
-                                </td>
-                                <td className='text-center'>
-                                    <div className='container mt-3'>
-                                        <ViewUserModel
-                                            uid={user._id}
-                                            firstName={user.firstName}
-                                            lastName={user.lastName}
-                                            email={user.email}
-                                            phone={user.phone}
-                                            dob={user.dateOfBirth}
-                                            status={user.status}
-                                            accountType={user.accountType}
-                                        />
-                                    </div>
-                                </td>
+                <div className='d-flex  justify-content-center'>
+                    <h2 className='m-3'>All Users</h2>
+                </div>
+
+                <div className='d-flex flex-wrap justify-content-center'>
+                    <Table striped bordered hover style={{ width: '75vw' }}>
+                        <thead>
+                            <tr>
+                                <th className='text-center'>First Name</th>
+                                <th className='text-center'>Last Name</th>
+                                <th className='text-center'>Email</th>
+                                <th className='text-center'>Phone Number</th>
+                                <th className='text-center'>Birth Day</th>
+                                <th className='text-center'>
+                                    <Form>
+                                        <Row>
+                                            <Col xs={10}>
+                                                <Form.Control
+                                                    onChange={(e) =>
+                                                        handleSearch(e)
+                                                    }
+                                                    name='item'
+                                                    placeholder='Search by email'
+                                                />
+                                            </Col>
+                                        </Row>
+                                    </Form>
+                                </th>
                             </tr>
-                        ))}
-                    </tbody>
-                    {/* <tfoot>
-                        <tr>
-                            <td colSpan={6}>
-                                <div className='m-2 d-flex justify-content-center'>
-                                    <ButtonGroup className='mb-2'>
-                                        <Button onClick={previousPage}>
-                                            &lt;Prev
-                                        </Button>
-                                        <Button>
-                                            {' '}
-                                            {pages.currentPage} of{' '}
-                                            {pages.totalPages}{' '}
-                                        </Button>
-                                        <Button onClick={nextPage}>
-                                            Next&gt;
-                                        </Button>
-                                    </ButtonGroup>
-                                </div>
-                            </td>
-                        </tr>
-                    </tfoot> */}
-                </Table>
+                        </thead>
+                        <tbody>
+                            {users.map((user) => (
+                                <tr key={user._id}>
+                                    <td className='text-center'>
+                                        {user.firstName}
+                                    </td>
+                                    <td className='text-center'>
+                                        {user.lastName}
+                                    </td>
+                                    <td className='text-center'>
+                                        {user.email}
+                                    </td>
+                                    <td className='text-center'>
+                                        {user.phone}
+                                    </td>
+                                    <td className='text-center'>
+                                        {dateConvert(user.dateOfBirth)}
+                                    </td>
+                                    <td className='text-center'>
+                                        <div className='container mt-3 mr-2'>
+                                            <ViewUserModel
+                                                uid={user._id}
+                                                firstName={user.firstName}
+                                                lastName={user.lastName}
+                                                email={user.email}
+                                                phone={user.phone}
+                                                dob={user.dateOfBirth}
+                                                status={user.status}
+                                                accountType={user.accountType}
+                                            />
+
+                                            <Button
+                                                variant='primary'
+                                                onClick={() =>
+                                                    deleteUser(user._id)
+                                                }
+                                            >
+                                                <CgTrash />
+                                            </Button>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
